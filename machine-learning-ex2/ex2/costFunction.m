52d2eb0db04928d9948d9a88f4349a56d809c554
lrt98802 <rtlin97@gmail.com>
2017-08-26 17:16:42 +0800

    Finish Computing Cost for Logistic Regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,8 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
+% Compute the cost
+h_theta_x = sigmoid(X * theta);
+J = sum(y .* log(h_theta_x) + (1-y) .* log(1-h_theta_x)) / (-m);
 
 
 
